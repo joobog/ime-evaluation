fa75003a12bdd0290ec647cd912eab12d9772085
Eugen Betke <betke@dkrz.de>
2018-10-21 18:22:48 +0200

    Results for 1 node for 1MB and 10MB

--- run.sh
+++ run.sh
@@ -7,11 +7,35 @@ function force_exit {
 
 trap force_exit SIGINT
 
+#    HOST_LIST=( \
+##isc17-c01 \
+#isc17-c02 \
+#isc17-c03 \
+#isc17-c04 \
+#isc17-c05 \
+#isc17-c06 \
+#isc17-c07 \
+#isc17-c09 \
+#isc17-c11 \
+#isc17-c12 \
+##isc17-c13 \
+##isc17-c14 \
+##isc17-c15 \
+##isc17-c18 \
+##isc17-c20 \
+##isc17-c22 \
+##isc17-c23 \
+#)
+
+# isc17-c19  offline
+# isc17-c10  offline
+# isc17-c08  MPI problems
+
 # Provides a list of good hosts (that contains QDR connection)
 function hosts() {
     num="$1"
-    #HOST_LIST=( isc17-c02 isc17-c03 isc17-c04 isc17-c05 isc17-c08 isc17-c09 isc17-c10 isc17-c12 isc17-c14 isc17-c23 )
-    HOST_LIST=( isc17-c01 isc17-c02 isc17-c03 isc17-c04 isc17-c05 isc17-c06 isc17-c07 isc17-c08 isc17-c09 isc17-c10 isc17-c11 isc17-c12 isc17-c13 isc17-c14 isc17-c15 isc17-c18 isc17-c19 isc17-c20 isc17-c22 isc17-c23 )
+    HOST_LIST=( isc17-c02 isc17-c04 isc17-c03 isc17-c05 isc17-c06 isc17-c07 isc17-c08 isc17-c09 isc17-c11 isc17-c12 isc17-c13 isc17-c14 isc17-c15 isc17-c18 isc17-c22 isc17-c01)
+
     hlist=${HOST_LIST[0]}
     for POS in $(seq 1 $(($num - 1))) ; do 
         hlist="$hlist,${HOST_LIST[$POS]}"
@@ -19,22 +43,23 @@ function hosts() {
     echo $hlist
 }
 
-
-export MODULEPATH=/gsfs/betke/software/modules:$MODULEPATH
+#export MODULEPATH=/gsfs/betke/software/modules:$MODULEPATH
+export MODULEPATH=/esfs/jtacquaviva/software/modules:$MODULEPATH
 
 module purge
 module load betke/hdf5/1.8.20-ddn
 module load betke/ior/git-ddn
 module list
 
-LUSTRE_TESTFILE="/esfs/jtacquaviva/iorperf/file"
+LUSTRE_TESTFILE="/esfs/jtacquaviva/iorperf/file2"
 ITERATIONS=3
 IOR="$(which ior)"
 MPIEXEC="/opt/ddn/mvapich/bin/mpiexec"
 
-NN_ARR=(1)
-PPN_ARR=(8 6 4 2 1)
-T_ARR=( $((16*1024)) $((100*1024)) $((1024*1024)) $((10240*1024)) )
+NN_ARR=(2)
+PPN_ARR=( 1 8 6 4 2 )
+#T_ARR=( $((16*1024)) $((100*1024)) $((1024*1024)) $((10240*1024)) )
+T_ARR=( $((1024*1024)) $((10240*1024)) )
 API_ARR=("MPIIO" "POSIX")
 
 for COUNT in $(seq 1); do
@@ -52,12 +77,12 @@ for API in ${API_ARR[@]}; do
         fi
         touch $BENCHFILE
 
-        IOR_PARAMS="-i $ITERATIONS -s 1 -t $T -b $((130 * 1024 * 1024 * 1020 / $PPN)) -o $LUSTRE_TESTFILE -a $API -e -g -z -k"
-        MPIEXEC_PARAMS=" -ppn $PPN -np $(($NN * $PPN)) -hosts $(hosts $NN)"
+        IOR_PARAMS="-i $ITERATIONS -s 1 -t $T -b $((132 * 1024 * 1024 * 1020 / $PPN)) -o $LUSTRE_TESTFILE -a $API -e -g -z -k"
+        MPIEXEC_PARAMS=" -ppn $PPN -np $(($NN * $PPN)) --hosts $(hosts $NN)"
 
         TESTDIR="$(dirname $LUSTRE_TESTFILE)"
         if [ -d $TESTDIR ]; then
-            rm $TESTDIR
+            rm -r $TESTDIR
         fi
         mkdir -p $TESTDIR 
         lfs setstripe	-c $((2 * $NN)) $TESTDIR
---
+++ show_node_status.sh
@@ -0,0 +1,63 @@
+#/bin/bash 
+
+function join_by() { 
+	local IFS="$1"; shift; echo "$*"; 
+}
+
+
+cx4nodes=( \
+isc17-c02 \
+isc17-c03 \
+isc17-c04 \
+isc17-c05 \
+isc17-c06 \
+isc17-c07 \
+isc17-c08 \
+isc17-c09 \
+isc17-c10 \
+isc17-c11 \
+isc17-c12 \
+isc17-c13 \
+isc17-c14 \
+isc17-c15 \
+isc17-c18 \
+isc17-c19 \
+isc17-c20 \
+isc17-c22 \
+isc17-c23 \
+isc17-c01 \
+)
+
+#cx4nodes=( isc17-c0{1..9} isc17-c{10..22} )
+node_list=$( join_by , "${cx4nodes[@]}" )
+
+
+IFS='' read -r -d '' script <<"EOF"
+if [ -d /esfs/jtacquaviva ]; then
+	echo 'ok'
+else 
+	echo "failed: $(hostname) /esfs/ is not mounted"
+fi
+EOF
+
+declare -a good_nodes
+for node in ${cx4nodes[@]}; do
+	#echo "ping $node"
+	status=$(ping -c1 $node | grep Unreachable)
+	if [[ "" == $status ]]; then
+		check=$(ssh $node "$script")
+		if [[ "ok" == ${check} ]]; then
+			good_nodes=( ${good_nodes[@]} $node )
+		else 
+			echo "$node failed with: $check"
+		fi
+	else 
+		echo "$node is unreachable"
+	fi
+done
+
+echo "${good_nodes[@]}"
+echo $( join_by , "${good_nodes[@]}" )
+
+
+
